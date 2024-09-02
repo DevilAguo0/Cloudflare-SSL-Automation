5e7facf4f6bb6c7662a172f557f33e9815e2867e
Yord YI <75577619+YordYI@users.noreply.github.com>
2024-09-03 05:27:54 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 下载脚本
+curl -o /usr/local/bin/yord-ssl-setup https://raw.githubusercontent.com/your-repo/yord-ssl-setup.sh
+
+# 添加执行权限
+chmod +x /usr/local/bin/yord-ssl-setup
+
+echo "Yord SSL Setup 已安装成功！运行 'yord-ssl-setup' 来使用。"
